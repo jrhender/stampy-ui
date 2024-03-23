6a43db7187b27d4363df177359d47777d8094e9e
Daniel O'Connell <github@ahiru.pl>
2024-03-23 21:45:43 +0100

    typos + title

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,28 +1,8 @@
-import type {LoaderFunction} from '@remix-run/cloudflare'
-import {ShouldRevalidateFunction, redirect} from '@remix-run/react'
-import {QuestionState} from '~/server-utils/stampy'
+import {ShouldRevalidateFunction} from '@remix-run/react'
 import {ContentBoxMain, ContentBoxSecond, ContentBoxThird} from '~/components/ContentBox'
 import useToC from '~/hooks/useToC'
 import Grid from '~/components/Grid'
 import Page from '~/components/Page'
-import {getStateEntries} from '~/hooks/stateModifiers'
-import {questionUrl} from '~/routesMapper'
-
-export const loader = async ({request}: Parameters<LoaderFunction>[0]) => {
-  const url = new URL(request.url)
-  const stateFromUrl = url.searchParams.get('state')
-  if (stateFromUrl) {
-    const firstOpenId = getStateEntries(stateFromUrl).filter(
-      ([_, state]) => state === QuestionState.OPEN
-    )[0]?.[0]
-    if (firstOpenId) {
-      url.searchParams.delete('state')
-      url.pathname = questionUrl({pageid: firstOpenId})
-      throw redirect(url.toString())
-    }
-  }
-  return null
-}
 
 export const shouldRevalidate: ShouldRevalidateFunction = () => false
 
@@ -31,7 +11,7 @@ export default function App() {
   return (
     <Page>
       <div className="page-body">
-        <h1 className="padding-bottom-80 padding-top-56">Answers on all things AI safety</h1>
+        <h1 className="padding-bottom-80 padding-top-56">Your guide to AI safety</h1>
 
         <ContentBoxMain />
         <ContentBoxSecond />
